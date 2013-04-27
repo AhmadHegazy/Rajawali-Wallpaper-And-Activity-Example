f512663e2467acf4259a2d8d3cad3317b165a560
Ian thomas <ian.shaun.thomas@gmail.com>
2013-04-27 17:16:25 -0400

    Updated to demonstrate object color issue.

--- res/raw/mat_texture.glsl
+++ res/raw/mat_texture.glsl
@@ -1,8 +1,10 @@
 precision mediump float;
 
-varying vec2 vTextureCoord; 
-uniform sampler2D uDiffuseTexture; 
+varying vec2 vTextureCoord;
 varying vec4 vColor;
+
+uniform sampler2D uDiffuseTexture;
+uniform sampler2D uAlphaTexture;
 
 void main() { 
 	gl_FragColor = texture2D(uDiffuseTexture, vTextureCoord) * vColor; 
--- src/com/testing/rajawali/MyRenderer.java
+++ src/com/testing/rajawali/MyRenderer.java
@@ -7,6 +7,7 @@ import rajawali.animation.Animation3D.RepeatMode;
 import rajawali.animation.ColorAnimation3D;
 import rajawali.materials.SimpleMaterial;
 import rajawali.materials.TextureManager.TextureType;
+import rajawali.math.Number3D;
 import rajawali.primitives.Plane;
 import rajawali.renderer.RajawaliRenderer;
 import android.content.Context;
@@ -15,9 +16,8 @@ public final class MyRenderer extends RajawaliRenderer {
 
 	private static final int SIMPLE_VERTEX_MATERIAL = com.monyetmabuk.livewallpapers.photosdof.R.raw.simple_material_vertex;
 
-	private Animation3D mAnimation;
 	private UserPrefs mUserPrefs;
-	private Plane box;
+	private Plane mPlaneLeft;
 
 	public MyRenderer(Context context) {
 		super(context);
@@ -30,63 +30,70 @@ public final class MyRenderer extends RajawaliRenderer {
 	@Override
 	protected void initScene() {
 		setFrameRate(60);
-		
+
 		final MySimpleMaterial material = new MySimpleMaterial();
 		material.setUseColor(true);
 
-		box = new Plane(1, 1, 1, 1);
-		box.setMaterial(material);
-		box.setTransparent(true);
-		box.setColor(0xffffff);
-		box.setX(-0.25f);
-		addChild(box);
+		mPlaneLeft = new Plane(1, 1, 1, 1);
+		mPlaneLeft.setMaterial(material);
+		mPlaneLeft.setTransparent(true);
+		mPlaneLeft.setColor(0xffffff);
+		mPlaneLeft.setX(-0.25f);
+		addChild(mPlaneLeft);
 
-		final Plane mPlane2 = new Plane(1, 1, 1, 1);
-		mPlane2.setMaterial(material);
-		mPlane2.setManageMaterial(false);
-		mPlane2.setTransparent(true);
-		mPlane2.setColor(0xaaff1111);
-		mPlane2.setZ(0.1f);
-		mPlane2.setX(0.25f);
-		addChild(mPlane2);
+		final MySimpleMaterial material2 = new MySimpleMaterial();
+		material2.setUseColor(true);
+
+		final Plane mPlaneRight = new Plane(1, 1, 1, 1);
+		mPlaneRight.setMaterial(material2);
+		mPlaneRight.setTransparent(true);
+		mPlaneRight.setColor(0xaaff1111);
+		mPlaneRight.setZ(0.1f);
+		mPlaneRight.setX(0.25f);
+		addChild(mPlaneRight);
+
+		final Number3D toPosition = new Number3D(mPlaneRight.getPosition());
+		toPosition.x = 2f;
 		
-		mAnimation = new ColorAnimation3D(0xaaff1111, 0xffffff11);
-		mAnimation.setDelay(1000);
-		mAnimation.setTransformable3D(mPlane2);
-		mAnimation.setDuration(2000);
-		mAnimation.setRepeatMode(RepeatMode.REVERSE_INFINITE);
-		registerAnimation(mAnimation);
-		mAnimation.play();
+		final Animation3D mAnimRightPlane;
+		mAnimRightPlane = new ColorAnimation3D(0xaaff1111, 0xaaffff11);
+		mAnimRightPlane.setDelay(1000);
+		mAnimRightPlane.setTransformable3D(mPlaneRight);
+		mAnimRightPlane.setDuration(2000);
+		mAnimRightPlane.setRepeatMode(RepeatMode.REVERSE_INFINITE);
+		registerAnimation(mAnimRightPlane);
+		mAnimRightPlane.play();
 
-		final MyTextureMaterial material2 = new MyTextureMaterial();
-		material2.addTexture(mTextureManager.addTexture(R.drawable.texture_normal));
+		final MyTextureMaterial material3 = new MyTextureMaterial();
+		material3.setUseColor(true);
 
-		final Plane mPlane3 = new Plane(1, 1, 1, 1);
-		mPlane3.setMaterial(material2);
-		mPlane3.setColor(0xaaffffff);
-		mPlane3.setTransparent(true);
-		mPlane3.setY(0.5f);
-		mPlane3.setRotZ(-90);
-		addChild(mPlane3);
+		final Plane mPlaneTopAndroid = new Plane(1, 1, 1, 1);
+		mPlaneTopAndroid.setMaterial(material3);
+		mPlaneTopAndroid.setColor(0xaa1111ff);
+		mPlaneTopAndroid.addTexture(mTextureManager
+				.addTexture(R.drawable.texture_normal));
+		mPlaneTopAndroid.setTransparent(true);
+		mPlaneTopAndroid.setY(0.5f);
+		mPlaneTopAndroid.setZ(0.01f);
+		addChild(mPlaneTopAndroid);
 
-		final SimpleMaterial material3 = new SimpleMaterial();
-		material3.addTexture(mTextureManager.addEtc1Texture(
-				new int[] { R.raw.texture_normal_mip_0,
-						R.raw.texture_normal_mip_1, R.raw.texture_normal_mip_2,
-						R.raw.texture_normal_mip_3, R.raw.texture_normal_mip_4,
-						R.raw.texture_normal_mip_5, R.raw.texture_normal_mip_6,
-						R.raw.texture_normal_mip_7 }, TextureType.DIFFUSE));
+		final SimpleMaterial material4 = new SimpleMaterial();
+		material4.addTexture(mTextureManager.addEtc1Texture(new int[] {
+				R.raw.texture_normal_mip_0, R.raw.texture_normal_mip_1,
+				R.raw.texture_normal_mip_2, R.raw.texture_normal_mip_3,
+				R.raw.texture_normal_mip_4, R.raw.texture_normal_mip_5,
+				R.raw.texture_normal_mip_6, R.raw.texture_normal_mip_7 },
+				TextureType.DIFFUSE));
 
-		final Plane mPlane4 = new Plane(1, 1, 1, 1);
-		mPlane4.setMaterial(material3);
-		mPlane4.setY(-0.5f);
-		mPlane4.setRotZ(-90);
-		addChild(mPlane4);
+		final Plane mPlaneBottomAndroid = new Plane(1, 1, 1, 1);
+		mPlaneBottomAndroid.setMaterial(material4);
+		mPlaneBottomAndroid.setY(-0.5f);
+		addChild(mPlaneBottomAndroid);
 	}
 
 	@Override
 	public void onDrawFrame(GL10 glUnused) {
-		box.setColor(mUserPrefs.getBoxColor());
+		mPlaneLeft.setColor(mUserPrefs.getBoxColor());
 		super.onDrawFrame(glUnused);
 	}
 
